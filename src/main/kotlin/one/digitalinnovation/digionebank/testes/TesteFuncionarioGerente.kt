38b2ea40dbae09e1f4dc6f0f7697f832d854d28f
Anderson Santiago <gsantiago.anderson@gmail.com>
2021-03-04 18:16:47 -0300

    Added authentication test

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,7 +3,8 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria José","12345678977",5000.0)
+    val maria = Gerente("Maria José","12345678977",5000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(maria)
 
+    TesteAutenticacao().autentica(maria)
 }
